88c695895c452a6a11cf287af88682e33b293630
bjrmatou <143578923+bjrmatou@users.noreply.github.com>
2023-09-27 22:51:46 +0200

    HOOK REUSSI HALLELUJAH

--- wp-content/themes/planty-astra/functions.php
+++ wp-content/themes/planty-astra/functions.php
@@ -7,3 +7,32 @@ function theme_enqueue_styles()
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
+
+
+
+/**********HOOK LIEN ADMIN NAVBAR *************************/
+
+add_filter( 'wp_nav_menu_items', 'add_menu_item', 10, 2 );
+function add_menu_item ( $items, $args ) 
+{        
+    $items_array = array();
+
+        if ( is_user_logged_in ( $item_pos = strpos ( $items, '<li', 2 ) ) ) 
+        {
+            $items_array[] = substr($items, 0, $item_pos);
+            $items = substr($items, $item_pos);
+        }
+
+        $items_array[] = $items;
+        array_splice($items_array, 1, 0, '<li class="menu-item-admin"><a href="' . admin_url() . '">Admin</a></li>'); // POSITION DE L'ITEM ici 1 donc
+
+        $items = implode('', $items_array);
+       
+       return $items;
+}
+
+
+
+
+?>
+
